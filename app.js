278b35617c3a54acf29468f74bc0a521afa1ba82
Phichet Liwvaha <phichet.pcl@gmail.com>
2024-05-03 18:44:53 +0700

    Add:runtime server

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
 const app = express();
+const port = 5000;
 
 // app.get('/', (req, res) => {
 //     res.send
 // })
 
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log('Server Runing');
 })
