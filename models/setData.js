2ca87e45a5bc1269bcfddb8918591b592f0ac317
Phichet Liwvaha <phichet.pcl@gmail.com>
2024-05-07 19:40:34 +0700

    Edit:chang spaces

--- models/setData.js
+++ models/setData.js
@@ -1,12 +1,12 @@
 const db = require("../connection/dbConnection");
 
 var setData = {
-    setDataParcel: function (callback) {
-        return db.query(
-            `SELECT * FROM pacel`
-        ),
-        callback
-    }
+  setDataParcel: function (callback) {
+    return db.query(
+      `SELECT * FROM pacel`
+    ),
+      callback
+  }
 }
 
 module.exports = setData;
