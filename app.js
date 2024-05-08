bd898ff4c6a1b868da656e7c2dc1c529349d9f48
Phichet Liwvaha <phichet.pcl@gmail.com>
2024-05-08 19:58:38 +0700

    Update:set route for service

--- app.js
+++ app.js
@@ -6,11 +6,21 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false,}));
 
+// require routes
+var setData = require("./routers/setData");
+var getData = require("./routers/getData");
+var deleteData = require("./routers/deleteData");
+
 // use routes
+app.use("/setData", setData);
+app.use("/getData", getData);
+app.use("/deleteData", deleteData);
+
+// default route
 app.get("/", (req, res) => {
     res.send("Hello! This is PSIP Project Server");
 });
-  
+
 // port setting 
 const port = process.env.PORT || 5000;
 app.listen(port, process.env.PORT, function(){
