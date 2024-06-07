f6e7aab4f34c51c9b09df3480d3ee18aa69ab7c4
Phichet Liwvaha <phichet.pcl@gmail.com>
2024-06-07 20:47:06 +0700

    Update: route upload file and update image on databast [PASS]

--- models/uploadFile.js
+++ models/uploadFile.js
@@ -1,18 +1,10 @@
 const db = require("../connection/dbConnection");
 
 var uploadFiles = {
-  updateImage: function (image_name, rfid, callback) {
+  updateImage: function (parcelID, fileImageName, callback) {
     return db.query(
-      "UPDATE `member` SET `image_file`= ? WHERE pacel_id = ? AND active = 1",
-      [image_name, pacel_id],
-      callback
-    );
-  },
-
-  getImageName: function (pacel_id, callback) {
-    return db.query(
-      "SELECT image_file FROM `member` WHERE pacel_id = ? AND active = 1",
-      [pacel_id],
+      "UPDATE `parcel` SET parcel.IMAGE_URL= ? WHERE parcel.ID = ? AND active = 1",
+      [fileImageName, parcelID],
       callback
     );
   },
--- routers/uploadFile.js
+++ routers/uploadFile.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const multer = require('multer');
 const router = express.Router();
+const uploadFile = require("../models/uploadFile");
 
+// begin multer config
+const multer = require('multer');
 const storage = multer.diskStorage({ 
   destination: function (req, file, cb) {
     cb(null, './uploads/')
@@ -10,11 +12,43 @@ const storage = multer.diskStorage({
     cb(null, Date.now() + ".png")
   }
 })
-
 const upload = multer({ storage: storage })
+// end multer config
 
-router.post('/uploadFile', upload.single('file'), (req, res) => { 
-  res.send(req.file) 
-})
+router.post('/uploadFile', upload.single('file'), async function (req, res) { 
+  let parcelID = req.body.parcelID;
+  let fileImageName = req.file.filename;
+  let dataResponse;
+
+  dataResponse = await updateImage(parcelID, fileImageName)
+
+  if (typeof dataResponse != "boolean") {
+    res.json({ status: "Failed", data: dataResponse });
+  } else if (dataResponse) {
+    res.json({ status: "Succeed", data: req.file });
+  } else {
+    res.json({ status: "Failed", data: "Error" });
+  }
+
+  // res.json({ status: "Succeed", data: req.file });
+});
+
+async function updateImage(parcelID, fileImageName) {
+  return new Promise((resolve, reject) => {
+    try {
+      uploadFile.updateImage(parcelID, fileImageName, (err, rows) => {
+        if (err) {
+          console.log(err);
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      resolve(false);
+    }
+  });
+}
 
 module.exports = router
